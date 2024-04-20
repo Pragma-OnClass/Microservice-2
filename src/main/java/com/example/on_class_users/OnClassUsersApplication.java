72460e94642040a122ce319fa127534188c6de4c
blessed0314 <christian.daniel999@gmail.com>
2024-04-19 19:31:39 -0500

    Se sube el esqueleto del segundo microservicio

---
+++ src/main/java/com/example/on_class_users/OnClassUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.on_class_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnClassUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnClassUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/on_class_users/OnClassUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.on_class_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnClassUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
